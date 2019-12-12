9f5eb3b20b9db07fd3cff721314b37619c359f3f
olympia <owojcik7@gmail.com>
2019-12-12 16:32:15 -0500

    Initial commit and add screens

---
+++ screens/CategoryMealScreen.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { View, Text, StyleSheet } from "react-native"
+
+const CategoryMealScreen = props => {
+    return (
+        <View style={styles.screen}><Text>The Categories Screen</Text></View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default CategoryMealScreen
---
+++ screens/MealDetailsScreen.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { View, Text, StyleSheet } from "react-native"
+
+const MealDetailsScreen = props => {
+    return (
+        <View style={styles.screen}><Text>The Meal Details Screen</Text></View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default MealDetailsScreen
