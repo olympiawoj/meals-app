23d22ac234af3c532e48e01f00f901359054d2d1
olympia <owojcik7@gmail.com>
2019-12-19 10:39:22 -0500

    Add Filtering Logic to actions and reducer

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,5 +1,6 @@
 import { MEALS } from "../../data/dummy-data"
-import { TOGGLE_FAVORITE } from "../actions/meals"
+import { TOGGLE_FAVORITE, SET_FILTERS } from "../actions/meals"
+
 
 const initialState = {
     meals: MEALS,
@@ -21,6 +22,28 @@ const mealsReducer = (state = initialState, action) => {
                 const meal = state.meals.find(meal => meal.id === action.mealId)
                 return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) }
             }
+
+        case TOGGLE_FAVORITE:
+            //extract filters setup by user
+            const appliedFilters = actions.filters
+            const updatedFilterMeals = state.meals.filter(meal => {
+                if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+                    return false
+                }
+                if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+                    return false
+                }
+                if (appliedFilters.vegetarian && !meal.isVegetarian) {
+                    return false
+                }
+                if (appliedFilters.vegan && !meal.isVegan) {
+                    return false
+                }
+                return true
+
+            })
+            return { ...state, filteredMeals: updatedFilterMeals }
+
         default:
             return state
     }
