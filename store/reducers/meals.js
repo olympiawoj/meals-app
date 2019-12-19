62d139e4b2dbcc0d35c2521ae9a9d799e5efd60d
olympia <owojcik7@gmail.com>
2019-12-19 11:41:08 -0500

    Fix bug in reducer to change case to SET_FILTERS

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -11,19 +11,19 @@ const initialState = {
 const mealsReducer = (state = initialState, action) => {
     switch (action.type) {
         case TOGGLE_FAVORITE:
-            const existingIndex = state.favoriteMeals.findIndex(meal => meal.id = action.mealId)
+            const existingIndex = state.favoriteMeals.findIndex(
+                meal => meal.id === action.mealId
+            );
             if (existingIndex >= 0) {
-                //create copy of array to not manipulate original
-                const updatedFavMeals = [...state.favoriteMeals]
-                updatedFavMeals.splice(existingIndex, 1)
-                return { ...state, favoriteMeals: updatedFavMeals }
+                const updatedFavMeals = [...state.favoriteMeals];
+                updatedFavMeals.splice(existingIndex, 1);
+                return { ...state, favoriteMeals: updatedFavMeals };
             } else {
-                //concat returns new array adding new item
-                const meal = state.meals.find(meal => meal.id === action.mealId)
-                return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) }
+                const meal = state.meals.find(meal => meal.id === action.mealId);
+                return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) };
             }
 
-        case TOGGLE_FAVORITE:
+        case SET_FILTERS:
             //extract filters setup by user
             const appliedFilters = actions.filters
             const updatedFilterMeals = state.meals.filter(meal => {
